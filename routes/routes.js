e2d9298f60af82a51cefe2be0d300efa22db799d
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-07-27 11:57:35 +0200

    static path add to fix 404 error

--- routes/routes.js
+++ routes/routes.js
@@ -5,14 +5,14 @@ const path = require('path')
 router.get('/', (req, res) => {
   //might not be the proper solution but it works :)
   // console.log(path.dirname(__dirname))
-  res.sendFile(path.dirname(__dirname) + "/home.html")
+  res.sendFile(path.dirname(__dirname) + "/views/home.html")
 })
 
 router.get('/register', (req, res) => {
-  res.sendFile(path.dirname(__dirname) + "/register.html")
+  res.sendFile(path.dirname(__dirname) + "/views/register.html")
 })
 
 router.get('/login', (req, res) => {
-  res.sendFile(path.dirname(__dirname) + "/login.html")
+  res.sendFile(path.dirname(__dirname) + "/views/login.html")
 })
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
-
+require('dotenv').config();
 const app = express();
 
 const routes = require('./routes/routes.js')
-
+console.log(__dirname)
+//took me a while to figure out the use of it 
+//but finally used static to fix the error while using 
+//javascript in html from other files 
+app.use('/public', express.static(__dirname + '/public'));
 const port = process.env.PORT || 3000;
 
-
+console.log(process.env.PORT)
 
 app.use('/', routes)
 
